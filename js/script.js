dee8fdc6f72a5706b2d2c1a337607bc73a145e1d
Mickael R <94828882+Micka0675@users.noreply.github.com>
2022-11-29 21:29:45 +0100

    Add changes

--- js/script.js
+++ js/script.js
@@ -3,6 +3,22 @@ var animateTitleLeft = document.getElementById('create2');
 var navBarMask = document.getElementById('maskNavbarBlur');
 var landing = document.getElementById('landing');
 var blocResp = document.getElementById('blocResp');
+var blocResp2 = document.getElementById('blocResp2');
+var titleResp = document.getElementById('titleResp');
+var animatetitle3 = document.getElementById('create3');
+var headReal = document.getElementsByClassName('head-real');
+var botReal = document.getElementsByClassName('bot-real');
+var blocReal = document.getElementsByClassName('bloc-real');
+var anime = [];
+var compteur = 0;
+
+for (i = 0; i < blocReal.length; i++ )
+{
+  anime.push(false);
+}
+
+console.log(headReal);
+// var botReal = document.getElementById('botReal');
 var lines = [] ;
 // var links = [];
 var lineText = document.getElementsByClassName('underlines');
@@ -110,7 +126,75 @@ document.addEventListener('scroll', function()
   {
     blocResp.classList.remove('bloc-anime-reveal');
   }
+  if(window.scrollY >= 2.7*window.innerHeight)
+  {
+    animatetitle3.classList.add('animate-header');
+    animatetitle3.classList.remove('hide-header');
+    blocResp2.classList.remove('bloc-anime-reveal');
+    blocResp2.classList.add('reals-shadow');
+    titleResp.classList.remove('bloc-anime-reveal');
+    titleResp.classList.add('reals-shadow');
+  }
+  else
+  {
+    animatetitle3.classList.add('hide-header');
+    animatetitle3.classList.remove('animate-header');
+    blocResp2.classList.add('bloc-anime-reveal');
+    blocResp2.classList.remove('reals-shadow');
+    titleResp.classList.add('bloc-anime-reveal');
+    titleResp.classList.remove('reals-shadow');
+  }
 });
+var rideau = false;
+var illus = false;
+
+for (i = 0; i < blocReal.length; i++ )
+{
+  blocReal[i].addEventListener('mouseover', function animeBloc(e)
+    {
+      compteur++;
+      for(j=0;j< blocReal.length;j++)
+      {
+          rideau = true
+          while(anime[j] === false && e.target === blocReal[j] && compteur < 2 && illus === false && rideau === true)
+          {
+            document.getElementsByClassName('head-real')[j].style.animation = 'headcomes 0.8s forwards';
+            document.getElementsByClassName('bot-real')[j].style.animation = 'botcomes 0.8s forwards';
+            anime[j] = true;
+            // e.target.children[1].classList.add('bot-real-alt');
+            // e.target.children[1].classList.remove('bot-real');
+          }
+      }
+      compteur = 0;
+      rideau = false;
+    } 
+  );
+};
+
+for (i = 0; i < blocReal.length; i++ )
+{
+  blocReal[i].addEventListener('mouseleave', function removeBloc(e)
+  {
+    compteur++;
+    for(j=0;j< blocReal.length;j++)
+    {
+      illus = true;
+        while(anime[j] === true && e.target === blocReal[j] && compteur < 2 && rideau === false && illus === true)
+        {
+          
+          document.getElementsByClassName('head-real')[j].style.animation = 'headremoves 0.8s forwards';
+          document.getElementsByClassName('bot-real')[j].style.animation = 'botremoves 0.8s forwards';
+          // e.target.children[1].classList.add('bot-real-alt');
+          // e.target.children[1].classList.remove('bot-real');
+          anime[j] = false;
+        }
+      
+    }
+    compteur = 0;
+    illus = false;
+  }
+  );
+}
 
 
 
